51f2288f69510ccabeb6f218e7ccb9a8881a0288
md-rejoyan-islam <rejoyanilsam0014@gmail.com>
2025-10-03 20:19:29 +0600

    jenkins pipeline2

--- app/(with-layout)/layout.tsx
+++ app/(with-layout)/layout.tsx
@@ -6,6 +6,7 @@ export default function Layout({
   readonly children: React.ReactNode;
 }) {
   console.log('Rendering Dashboard Layout');
+  console.log('Rendering Dashboard Layout');
 
   return <DashboardLayout>{children}</DashboardLayout>;
 }
